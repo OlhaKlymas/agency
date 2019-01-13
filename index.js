5cf708e9e3d671f8dbac545aefaac045a7086c88
OlhaKlymas <olhaklymas@gmail.com>
2019-01-13 21:00:54 +0200

    owl

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
-$(".owl-carousel").owlCarousel();
+// Owl Carousel
+$(function() {
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 3,
+    margin: 10,
+    loop: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 2000,
+  });
+});
+
+$(".next_button").click(function(){
+  owl.trigger("next.owl.carousel");
+});
+ 
+$(".prev_button").click(function(){
+  owl.trigger("prev.owl.carousel");
 });
