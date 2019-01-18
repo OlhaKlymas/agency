96d407a7580d0200f202315a852e52f59ffecd29
OlhaKlymas <olhaklymas@gmail.com>
2019-01-18 18:01:58 +0200

    add arrows

--- index.js
+++ index.js
@@ -12,11 +12,3 @@ $(function() {
     center:true,
   });
 });
-
-$(".next_button").click(function(){
-  owl.trigger("next.owl.carousel");
-});
- 
-$(".prev_button").click(function(){
-  owl.trigger("prev.owl.carousel");
-});
